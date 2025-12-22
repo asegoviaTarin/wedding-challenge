d353d171f030d19602c8361adbb345f11fc7ca90
alseta <asegovia@allfunds.com>
2025-12-22 18:38:42 +0100

    Fix missing databaseURL

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyAW7qpeyZX6yZq1XitFemhelUHytAA4MwU",
   authDomain: "wedding-app-1acc9.firebaseapp.com",
+  databaseURL: "https://wedding-app-1acc9-default-rtdb.firebaseio.com",
   projectId: "wedding-app-1acc9",
   storageBucket: "wedding-app-1acc9.firebasestorage.app",
   messagingSenderId: "32926774782",
